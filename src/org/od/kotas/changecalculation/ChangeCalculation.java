e84e3c47e5b5beb5235c2b7f5c7e4ce6fcc0349a
unknown <Michael-Weir@CNU407BQ21.na.odcorp.net>
2015-11-16 10:00:03 -0500

    Created initial kata for change testing

---
+++ src/org/od/kotas/changecalculation/ChangeCalculation.java
@@ -0,0 +1,15 @@
+package org.od.kotas.changecalculation;
+
+public class ChangeCalculation {
+
+	public static Change getCoinsFor(double d) {
+		Change change= new Change();
+		change.setNumberPennies(1);
+		// TODO Auto-generated method stub
+		return change;
+	}
+
+
+	
+	
+}
